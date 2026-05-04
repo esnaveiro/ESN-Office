e10a99c9a19be5a1b54732ea497b9f96f1b9605b
ddias <ddias@ua.pt>
2026-05-04 19:00:30 +0100

    chore: update eslint config and lock file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,15 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["warn", {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+      }],
+    },
+  },
+  {
     ignores: [
       "node_modules/**",
       ".next/**",
